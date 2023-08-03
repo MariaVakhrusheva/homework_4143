e524a0bb7316ab77e24094fd3b11335e2ed1f382
Marya <2510marycat@gmail.com>
2023-08-04 00:59:48 +0500

    Задача №41, №43

---
+++ Task_41/Program.cs
@@ -0,0 +1,29 @@
+﻿﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void CheckPlus(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) count++;
+    Console.Write($"{String.Join(", ", array)} -> {count}");
+}
+
+int[] CreateArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+        array[i] = GetInput("Вводите эти числа по одному: ");
+    return array;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetInput("Сколько чисел Вы хотите ввести? : ");
+int[] array = CreateArray(number);
+CheckPlus(array);
---
+++ Task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void FindPoint(int b1, int k1, int b2, int k2)
+{
+    if (k1 == k2) Console.Write("Прямые параллельны");
+    else
+    {
+        int x = (b2 - b1) / (k1 - k2);
+        int y = k1 * x + b1;
+        Console.Write($"Точка пересечения прямых X: {x}, Y: {y}");
+    }
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int b1 = GetInput("Введите значение b1: ");
+int k1 = GetInput("Введите значение k1: ");
+int b2 = GetInput("Введите значение b2: ");
+int k2 = GetInput("Введите значение b2: ");
+FindPoint(b1, k1, b2, k2);
